0b0675f6e9d2ec7536f45bf4b7b3f912c35beaa3
Your Name <desireirankundawilliams@gmail.com>
2021-10-05 22:28:48 +0200

    modified the header with flex

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,13 +1,22 @@
 import { AppBar, Toolbar, Typography } from '@material-ui/core'
 import React from 'react'
+import AccountCircleRoundedIcon from '@material-ui/icons/AccountCircleRounded';
+import { makeStyles } from '@material-ui/core';
 
+const  useStyles = makeStyles({
+    TypographyStlyes: {
+        flex: 1
+    }
+})
 const Header = () => {
+    const classes = useStyles(); 
     return (
         <AppBar position=" static" >
             <Toolbar>
-            <Typography>
-                This is our header
+            <Typography className={classes.TypographyStlyes} >
+                Material-Ui
             </Typography>
+            <AccountCircleRoundedIcon />
             </Toolbar>
         </AppBar>
     )
